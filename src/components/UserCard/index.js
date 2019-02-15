4383286d70b648ec1e6448e675aef8b94a2cb519
batko_zeus <batko_zeus@i.ua>
2019-02-15 17:13:24 +0200

    added lazy load on UserCard component

--- src/components/UserCard/index.js
+++ src/components/UserCard/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import classes from './UserCard.module.css';
 
-const UserCard = ({ first_name = '', last_name = '', avatar}) => (
+
+const UserCard = ({ first_name = '', last_name = '', avatar}) => () => (
   <div className={classes.container}>
     <img src={avatar} alt="avatar" className={classes.image} />
     <h1 className={classes.name}>{first_name}</h1>
@@ -9,4 +10,25 @@ const UserCard = ({ first_name = '', last_name = '', avatar}) => (
   </div>
 );
 
-export default UserCard;
+const getLazyComponent = (props) =>
+  lazy(() =>
+    new Promise((resolve) => {
+      resolve({
+        default: UserCard(props)
+      });
+    }
+  )
+);
+
+function ProgressiveUserCard(props) {
+  const SimpleUserCard = UserCard(props);
+  const LazyUserCard = getLazyComponent(props);
+
+  return (
+    <Suspense fallback={<SimpleUserCard />}>
+      <LazyUserCard />
+    </Suspense>
+  );
+}
+
+export default ProgressiveUserCard;
